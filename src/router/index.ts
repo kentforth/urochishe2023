c1e52480363ef4a9c4d35313b55d2d44362627eb
Konstantin Rusanov <rusanov@uteka.ru>
2023-05-20 17:58:07 +0700

    Display document title

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,18 +5,18 @@ const router = createRouter({
   routes: [
     {
       path: '/',
+      meta: {
+        title: 'Главная'
+      },
       name: 'home',
       component: () => import('../views/HomeView.vue')
     }
-    /* {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }*/
   ]
+})
+
+router.beforeEach((to: any, from, next) => {
+  document.title = to.meta.title
+  next()
 })
 
 export default router
