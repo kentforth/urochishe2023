7468b455bd3117a9b2fcd9edb94a760349f0576a
Konstantin Rusanov <rusanov@uteka.ru>
2023-05-24 00:22:26 +0700

    Remove Maska from global import

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import Maska from 'maska'
 import JsonExcel from 'vue-json-excel3'
 
 const app = createApp(App)
